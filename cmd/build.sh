e96e3ce2c49b5f67bf4849e3343c00d1b17074ef
Marky Egebäck <marky@egeback.se>
2020-06-28 16:31:10 +0000

    Fix docker file to install correct version of svtplay-dl

--- cmd/build.sh
+++ cmd/build.sh
@@ -2,4 +2,4 @@
 #version=0.1.3
 version=`cat VERSION`
 time=$(date)
-go build -o main -ldflags="-X 'github.com/egeback/play_media_api/internal/version.BuildTime=$time' -X 'github.com/egeback/play_media_api/internal/version.BuildVersion=$version'" ./internal/main.go
+go build -o mediadownloader -ldflags="-X 'github.com/egeback/mediadownloader/internal/version.BuildTime=$time' -X 'github.com/egeback/mediadownloader/internal/version.BuildVersion=$version'" ./cmd/mediadownloader/main.go
